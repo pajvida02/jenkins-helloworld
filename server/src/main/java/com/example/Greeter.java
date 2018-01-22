59bb99274ec5f0c2a9053442ca0c8e3acc3bfaa3
dev1 <dev1@wst.com>
2018-01-22 22:40:31 +0530

    fixed warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
 /**
- * This is a method.
+ * @param name of a person
+ * @return greeting string
  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
