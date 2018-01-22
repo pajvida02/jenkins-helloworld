4d0f0e06858a09f1e8e1136d8192119aaf8f21ce
dev1 <dev1@wst.com>
2018-01-22 22:48:24 +0530

    git greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
 /**
- * @param name of a person
+ * @param someone name of a person
  * @return greeting string
  */
   public final String greet(final String someone) {
